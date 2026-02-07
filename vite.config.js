9c6a5ce17550e60a2307f8e56ee6f6710ea87e40
narayana ravikumar <narayanaravikumar@narayanas-Laptop.local>
2026-02-07 09:29:13 -0800

    chore: configure custom domain nani.info

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/nani-site/', // Base path for custom domain
+    base: '/', // Base path for custom domain
 })
