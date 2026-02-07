6f332af19957f8ed8f0e7fe81bb954fd0ea647ab
narayana ravikumar <narayanaravikumar@narayanas-Laptop.local>
2026-02-07 08:21:46 -0800

    chore: configure deployment and remove CNAME

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/', // Base path for custom domain
+    base: '/nani-site/', // Base path for custom domain
 })
